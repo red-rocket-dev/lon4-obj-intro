3d711a1630553748716445ce11d054be07e95d0e
Jakub <kubplon@gmail.com>
2020-05-23 12:59:40 +0200

    Initial commit

---
+++ src/pl/sda/Main.java
@@ -0,0 +1,17 @@
+package pl.sda;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        Naszym zadaniem jest stworzenie kodu obsługującego bankomat. Powinien obsługiwać akcje takie jak:
+        * Wpłacenie gotówki
+        * Wybranie gotówki
+        * Sprawdzenie stanu konta
+         */
+        // Klasy:
+        // CashMachine
+        // Account
+        // Card (karta jest połączo:na z jednym kontem)
+    }
+}
