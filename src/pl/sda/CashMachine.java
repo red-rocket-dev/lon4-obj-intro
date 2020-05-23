39ed6965ae36350bfa67fa7b65a3a848003bd6c5
Jakub <kubplon@gmail.com>
2020-05-23 13:23:35 +0200

    cashMachine and card created

---
+++ src/pl/sda/CashMachine.java
@@ -0,0 +1,9 @@
+package pl.sda;
+
+import java.math.BigDecimal;
+
+public class CashMachine {
+    public BigDecimal checkBalance(String pin, Card card) {
+
+    }
+}
--- src/pl/sda/Main.java
+++ src/pl/sda/Main.java
@@ -1,5 +1,8 @@
 package pl.sda;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -13,7 +16,18 @@ public class Main {
         // CashMachine
         // Account
         // Card (karta jest połączo:na z jednym kontem)
+        //TODO: cwiczenia z bigdecimal
         double test = 1.03 - 0.42;
+//12-12-2026
+        Account account = new Account("Jakub",
+                "Płonka",
+                "PL41109024027856121167925782",
+                BigDecimal.valueOf(112));
+        new Card("4532988042633987",
+                "VISA",
+                LocalDate.of(2026, 12, 12),
+                "1234",
+                account);
         System.out.println(test);
     }
 }
